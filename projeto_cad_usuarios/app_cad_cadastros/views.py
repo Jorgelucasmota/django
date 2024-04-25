860f4108412af63b41f9a8589a4f6fa6ff0ff5a5
Jorge Lucas Mota Sousa <jorgelucas.mota13@gmail.com>
2024-04-25 09:48:23 -0300

    Cadastro

--- projeto_cad_usuarios/app_cad_cadastros/views.py
+++ projeto_cad_usuarios/app_cad_cadastros/views.py
@@ -6,5 +6,12 @@ def home(request):
 
 def usuarios(request):
     novo_usuario = Usuario()
-    nome = request.POST.get('nome')
-    idade = request.POST.get('idade')
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.idade = request.POST.get('idade')
+    novo_usuario.save()
+
+    usuarios = {
+        'usuarios' : Usuario.objects.all()
+    }
+
+    return render(request,'usuarios/usuarios.html',usuarios)
